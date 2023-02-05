8bfd125a0240cb662fc075f07e9ca3aaadedb8be
jesse <103243775+jessecoward@users.noreply.github.com>
2023-02-05 15:09:18 +0700

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>jie </h1>
+      <p>Welcome to my Next.js website</p >
+    </div>
+  )
+}
+
+export default IndexPage
