521cbf16e8150bd32d2fad5d31a2c7c1b70b1085
jesse <103243775+jessecoward@users.noreply.github.com>
2023-02-05 14:52:46 +0700

    1

--- my-app/src/pages/404.js
+++
@@ -1,7 +0,0 @@
-// pages/404.js
-
-import React from 'react'
-
-export default function Custom404() {
-  return <div>404 - Page not found</div>
-}
--- nextjs-blog/src/app/page.tsx
+++ nextjs-blog/src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const IndexPage = () => {
   return (
     <div>
-      <h1>jie</h1>
+      <h1>jie </h1>
       <p>Welcome to my Next.js website</p >
     </div>
   )
